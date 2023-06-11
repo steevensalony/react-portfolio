38c1e7eae00410784b6559f91edb9db4df403fbc
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-06-10 20:14:01 -0400

    Added routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Resume from "./components/Resume";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route exact path='/' element={<About />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/resumes' element={<Resume />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
-
-export default App;
