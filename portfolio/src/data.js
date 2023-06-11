7e684d9c7dbd07171e794d9c34a38844148de288
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-06-10 20:00:27 -0400

    Created data for project, resume and skills

---
+++ portfolio/src/data.js
@@ -0,0 +1,69 @@
+export const projects = [
+  {
+    title: "Code Quiz",
+    subtitle: "JavaScript",
+    description:
+      "A timed coding quiz application with multiple choice questions.",
+    image: "./codeQuiz.png",
+    link: "https://steevensalony.github.io/code-quiz/",
+  },
+  {
+    title: "FakeBook",
+    subtitle: "JavaScript and Handlebars",
+    description:
+      "A Social media app that allows users to interact with each other",
+    image: "./fakebook.jpg",
+    link: "https://the-fakebook-p2.herokuapp.com/",
+  },
+  {
+    title: "Top Movies",
+    subtitle: "JavaScript & HTML",
+    description:
+      "A website that shows the list of top movies along with their ratings.",
+    image: "./Capture.PNG",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Weather Dashboard",
+    subtitle: "JavaScript & HTML",
+    description:
+      "A weather application that display the weather.",
+    image: "./weather.png",
+    link: "https://steevensalony.github.io/weather-dashboard/",
+  },
+  {
+    title: "Work Day Scheduler",
+    subtitle: "JavaScript, HTML & CSS",
+    description:
+      "A calendar app where users to save events for each hour of a work day",
+    image: "./workDay.png",
+    link: "https://steevensalony.github.io/work-day-scheduler/",
+  },
+  {
+    title: "Text Editor",
+    subtitle: "JavaScript, HTML & CSS",
+    description:
+      "A Text editor application that runs in the browser. ",
+    image: "./TextEditor.png",
+    link: "https://gentle-brushlands-09588.herokuapp.com/",
+  },
+];
+
+export const Resume = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "./resume.png",
+    name: "Steeven salony",
+    company: "Resume",
+  }
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "MySql",
+  "Node",
+  "GraphQL",
+  "MongoDB",
+];
